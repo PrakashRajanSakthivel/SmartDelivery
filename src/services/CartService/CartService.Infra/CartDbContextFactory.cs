7e82f43f7370fd814b7dbd9976db85f62871ebf7
PrakashRajanSakthivel <45005244+PrakashRajanSakthivel@users.noreply.github.com>
2025-08-27 11:00:15 +0530

    remvoed unused values (#3)
    Co-authored-by: vibecode-lab <alltrialthings@gmail.com>

--- src/services/CartService/CartService.Infra/CartDbContextFactory.cs
+++ src/services/CartService/CartService.Infra/CartDbContextFactory.cs
@@ -2,8 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
-using Microsoft.Extensions.Configuration.EnvironmentVariables;
 
 namespace CartService.Infra
 {
